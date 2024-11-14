20965fd9e1e484ba6bbe2c77c4fefec707172eed
Anne-Sophie Cortez <aspintocortez@gmail.com>
2024-11-14 10:03:02 +0100

    resto

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+Restaurant.create(name: "L'échappée", address: "Paris", phone_number: "0644556677", category: "french")
+Restaurant.create(name: "La Fourmi", address: "Nantes", phone_number: "0644506677", category: "chinese")
+Restaurant.create(name: "Les soeurs", address: "Paris", phone_number: "0644756677", category: "italian")
+Restaurant.create(name: "Poulpe", address: "Rennes", phone_number: "0644556677", category: "french")
+Restaurant.create(name: "Agar", address: "Paris", phone_number: "0644556477", category: "french")
